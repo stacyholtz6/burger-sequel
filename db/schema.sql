f45365e2615204f46a030c749eb035fed757ec66
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-22 14:53:33 -0700

    updated readme

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### SCHEMA
 
--- CREATE DATABASE burgers_db;
+CREATE DATABASE sequelizedBurgers_db;
 
--- USE burgers_db;
+USE sequelizedBurgers_db;
 
--- CREATE TABLE burgers (
---   id int NOT NULL AUTO_INCREMENT,
---   burger_name varchar(255) NOT NULL,
---   devoured BOOLEAN DEFAULT FALSE,
---   PRIMARY KEY (id)
--- );
+CREATE TABLE burgers (
+  id int NOT NULL AUTO_INCREMENT,
+  burger_name varchar(255) NOT NULL,
+  devoured BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (id)
+);
