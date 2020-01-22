34df35df8f845ce4ab273e947358bd2e90e9529b
Stacy Holtz <stacyholtz6@gmail.com>
2020-01-22 10:05:44 -0700

    added form validation

--- models/burger.js
+++ models/burger.js
@@ -1,12 +1,19 @@
 module.exports = function (sequelize, DataTypes) {
 
   var Burger = sequelize.define('Burger', {
-    burger_name: DataTypes.STRING,
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1, 20]
+      }
+    },
+
     devoured: {
       type: DataTypes.BOOLEAN,
       defaultValue: false
     }
-  })
+  });
   return Burger;
-}
+};
 
